bbc171e1ccacebc6b85cf1d080bbf84a0e3bb1a6
Jacques <941214+jacola@users.noreply.github.com>
2022-10-31 17:01:46 +0900

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Insomnia Plugin GUID
+
+===
+
+Generate guids for the Insomnia REST client.
---
+++ app.js
@@ -0,0 +1,18 @@
+function uuidv4() {
+  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+    (
+      c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+    ).toString(16)
+  );
+}
+
+module.exports.templateTags = [
+  {
+    displayName: "GUID",
+    name: "guid",
+    description: "Generate a GUID",
+    args: [],
+    run: () => uuidv4(),
+  },
+];
